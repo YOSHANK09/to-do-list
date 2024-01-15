8318c1267d8af4ef785194c0fa8600e98ab1657c
Yoshank <118330522+YOSHANK09@users.noreply.github.com>
2024-01-15 23:55:36 +0530

    script.js

--- todo/script.js
+++ todo/script.js
@@ -1,4 +1,4 @@
-var todoList = []
+var todoList = [];
 var comdoList = [];
 var remList = [];
 var addButton = document.getElementById("add-button")
@@ -7,15 +7,22 @@ var deleteAllButton = document.getElementById("delete-all")
 var allTodos = document.getElementById("all-todos");
 var deleteSButton = document.getElementById("delete-selected")
 
+// Load data from local storage on page load
+window.addEventListener('load', function () {
+    if (localStorage.getItem('todoList')) {
+        todoList = JSON.parse(localStorage.getItem('todoList'));
+        update();
+        addinmain(todoList);
+    }
+});
 
-//event listners for add and delete
-addButton.addEventListener("click", add)
-deleteAllButton.addEventListener("click", deleteAll)
-deleteSButton.addEventListener("click", deleteS)
+// Event listeners for add and delete
+addButton.addEventListener("click", add);
+deleteAllButton.addEventListener("click", deleteAll);
+deleteSButton.addEventListener("click", deleteS);
 
-
-//event listeners for filtersk
-document.addEventListener('click', (e) => {
+// Event listeners for filters
+document.addEventListener('click', function (e) {
     if (e.target.className.split(' ')[0] == 'complete' || e.target.className.split(' ')[0] == 'ci') {
         completeTodo(e);
     }
@@ -31,32 +38,26 @@ document.addEventListener('click', (e) => {
     if (e.target.id == "com") {
         viewCompleted();
     }
-
 })
-//event listner for enter key
-todoInput.addEventListener('keypress', (e) => {
+
+// Event listener for Enter key
+todoInput.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         add();
     }
 });
 
-
-//updates the all the remaining, completed and main list
+// Updates all the remaining, completed, and main list
 function update() {
-    comdoList = todoList.filter((ele) => {
-        return ele.complete
-
-    })
-    remList = todoList.filter((ele) => {
-        return !ele.complete
-    })
+    comdoList = todoList.filter((ele) => ele.complete);
+    remList = todoList.filter((ele) => !ele.complete);
     document.getElementById("r-count").innerText = todoList.length.toString();
     document.getElementById("c-count").innerText = comdoList.length.toString();
-
+    // Save to local storage
+    localStorage.setItem('todoList', JSON.stringify(todoList));
 }
 
-//adds the task in main list
-
+// Adds the task in the main list
 function add() {
     var value = todoInput.value;
     if (value === '') {
@@ -68,17 +69,14 @@ function add() {
         id: Date.now().toString(),
         complete: false,
     });
-
     todoInput.value = "";
     update();
     addinmain(todoList);
 }
 
-
-//renders the main list and views on the main content
-
+// Renders the main list and views on the main content
 function addinmain(todoList) {
-    allTodos.innerHTML = ""
+    allTodos.innerHTML = "";
     todoList.forEach(element => {
         var x = `<li id=${element.id} class="todo-item">
     <p id="task"> ${element.complete ? `<strike>${element.task}</strike>` : element.task} </p>
@@ -86,83 +84,58 @@ function addinmain(todoList) {
                 <button class="complete btn btn-success">
                     <i class=" ci bx bx-check bx-sm"></i>
                 </button>
-
                 <button class="delete btn btn-error" >
                     <i class="di bx bx-trash bx-sm"></i>
                 </button>
             </div>
-        </li>`
-        allTodos.innerHTML += x
+        </li>`;
+        allTodos.innerHTML += x;
     });
 }
 
-
-//deletes and indiviual task and update all the list
+// Deletes an individual task and updates all the lists
 function deleteTodo(e) {
     var deleted = e.target.parentElement.parentElement.getAttribute('id');
-    todoList = todoList.filter((ele) => {
-        return ele.id != deleted
-    })
-
+    todoList = todoList.filter((ele) => ele.id != deleted);
     update();
     addinmain(todoList);
-
 }
 
-//completes indiviaula task and updates all the list
+// Completes an individual task and updates all the lists
 function completeTodo(e) {
     var completed = e.target.parentElement.parentElement.getAttribute('id');
     todoList.forEach((obj) => {
         if (obj.id == completed) {
-            if (obj.complete == false) {
-                obj.complete = true
-                e.target.parentElement.parentElement.querySelector("#task").classList.add("line");
-            } else {
-                obj.complete = false
-
-                e.target.parentElement.parentElement.querySelector("#task").classList.remove("line");
-            }
+            obj.complete = !obj.complete;
         }
     })
-
     update();
     addinmain(todoList);
 }
 
-
-//deletes all the tasks
-function deleteAll(todo) {
-
-    todoList = []
-
+// Deletes all the tasks
+function deleteAll() {
+    todoList = [];
     update();
     addinmain(todoList);
-
 }
 
-//deletes only completed task
-function deleteS(todo) {
-
-    todoList = todoList.filter((ele) => {
-        return !ele.complete;
-    })
-
-
+// Deletes only completed tasks
+function deleteS() {
+    todoList = todoList.filter((ele) => !ele.complete);
     update();
     addinmain(todoList);
-
 }
 
-
-// functions for filters
+// Functions for filters
 function viewCompleted() {
     addinmain(comdoList);
 }
 
 function viewRemaining() {
-
     addinmain(remList);
 }
+
 function viewAll() {
     addinmain(todoList);
 }
